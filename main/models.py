32093989bf10df795a85c69c0cb0af225a237e22
venuzzi <rifqipratama2004@gmail.com>
2023-11-14 21:44:10 +0700

    redeploy

--- main/models.py
+++ main/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+# Class Product
 class Product(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     date_added = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
+
+# Class Employee
+class Employee(models.Model):
+    department = models.CharField(max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
