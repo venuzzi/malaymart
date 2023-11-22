0ee323d3db2a1049913d22e50db278f4868f8f03
venuzzi <rifqipratama2004@gmail.com>
2023-11-22 09:09:42 +0700

    CSRF fix

--- malaymart/settings.py
+++ malaymart/settings.py
@@ -147,3 +147,5 @@ CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 SESSION_COOKIE_SAMESITE = 'None'
+
+CSRF_TRUSTED_ORIGINS = ["https://rifqi-pratama21-tugas.pbp.cs.ui.ac.id"]
